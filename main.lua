f55682989f880257a4712e709e682834785f3a2e
kmenu67 <haokhang861@gmail.com>
2026-01-30 10:50:20 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 print("K menu loaded OK")
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kmenu67/Kmenu1/main/main.lua"))()
 pcall(function()
     game.StarterGui:SetCore("SendNotification",{
         Title = "K Menu";
