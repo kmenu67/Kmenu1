6f2916ce97bfa665ce590e2efa225a03411217fe
kmenu67 <haokhang861@gmail.com>
2026-01-30 10:47:03 +0700

    Update main.lua
    loadstring(game:HttpGet("https://raw.githubusercontent.com/kmenu67/Kmenu1/main/main.lua"))()

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kmenu67/Kmenu1/main/main.lua"))()
+print("K menu loaded OK")
+
+pcall(function()
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "K Menu";
+        Text = "Script đã load thành công";
+        Duration = 5;
+    })
+end)
